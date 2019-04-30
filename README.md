ddb146fbaed6fb56102031dc5c97729aedda9dc9
qq1318944013 <36846733+qq1318944013@users.noreply.github.com>
2019-04-30 12:30:33 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Distributor  
+
+### 介绍
+实现常用的分布式组件
+
+### 开源地址
+
+
+
+### 实现功能
+### 
+
+### 友情链接
+ + [水不要鱼 & GitHub](https://github.com/FishGoddess)
+
+ + [Mackyhuang & GitHub](https://github.com/Mackyhuang)
+
+ + [MaDao & GitHub](https://github.com/Madaovo)
---
+++ src/test/java/wang/reder/AppTest.java
@@ -0,0 +1,20 @@
+package wang.reder;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
