9352314db7ee278f305b9bdb8e62c99ac26b7076
qq1318944013 <36846733+qq1318944013@users.noreply.github.com>
2019-05-04 22:44:54 +0800

    分布式缓存准备

---
+++ src/main/java/wang/reder/distributor/cache/IRedisCache.java
@@ -0,0 +1,11 @@
+package wang.reder.distributor.cache;
+
+/**
+ * <p>Redis缓存操作接口<p/>
+ *
+ * @author Red
+ * email: 1318944013@qq.com
+ * date: 2019/5/4 22:36
+ */
+public interface IRedisCache {
+}
